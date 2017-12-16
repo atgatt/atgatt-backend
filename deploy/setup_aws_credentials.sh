03ab7ada7ad7bb4c7f382d57e022031d5833116f
Benjamin Katz <bakatz91@gmail.com>
2017-12-15 23:07:40 -0800

    Cleans up setup script, adds checkout step so that the setup script can be called

--- deploy/setup_aws_credentials.sh
+++ deploy/setup_aws_credentials.sh
@@ -2,12 +2,5 @@
 set -x
 set -e
 
-AWS_CONFIG_FILE=$HOME/.aws/config
-
-mkdir $HOME/.aws
-touch $AWS_CONFIG_FILE
-chmod 600 $AWS_CONFIG_FILE
-
-echo "[profile eb-cli]"                              > $AWS_CONFIG_FILE
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
+aws configure --profile eb-cli set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure --profile eb-cli set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
