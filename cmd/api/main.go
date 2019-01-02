132ced3b76488f062b3627a526c1f671a88a29e5
Benjamin Katz <bakatz91@gmail.com>
2019-01-02 01:35:42 -0800

    Temporarily disables bg worker, fixes port settings, adds missing cron.yaml file to circleci settings

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"crashtested-backend/api"
 	"crashtested-backend/api/settings"
+	"os"
 )
 
 func main() {
-	server := &api.Server{Port: ":5000", Name: "crashtested-api", Version: "1.0.0", BuildNumber: "{LOCAL-DEV-BUILD}", CommitHash: "{LOCAL-DEV-COMMIT}", Settings: settings.GetSettingsFromEnvironment()}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	server := &api.Server{Port: ":" + port, Name: "crashtested-api", Version: "1.0.0", BuildNumber: "{LOCAL-DEV-BUILD}", CommitHash: "{LOCAL-DEV-COMMIT}", Settings: settings.GetSettingsFromEnvironment()}
 	server.Bootstrap()
 }
--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"crashtested-backend/worker"
 	"crashtested-backend/worker/settings"
-
-	// Importing the PostgreSQL driver with side effects because we need to call sql.Open() to run queries
-	_ "github.com/lib/pq"
+	"os"
 )
 
 func main() {
-	server := &worker.Server{Port: ":80", Name: "crashtested-worker", Version: "1.0.0", BuildNumber: "{LOCAL-DEV-BUILD}", CommitHash: "{LOCAL-DEV-COMMIT}", Settings: settings.GetSettingsFromEnvironment()}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	server := &worker.Server{Port: ":" + port, Name: "crashtested-worker", Version: "1.0.0", BuildNumber: "{LOCAL-DEV-BUILD}", CommitHash: "{LOCAL-DEV-COMMIT}", Settings: settings.GetSettingsFromEnvironment()}
 	server.Bootstrap()
 }
