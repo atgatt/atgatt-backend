1cd578fe741460581364fbd9cd17fa9b7e29847b
Benjamin Katz <bakatz91@gmail.com>
2019-05-18 11:44:04 -0700

    Removes outdated TODO comment

--- persistence/entities/product.go
+++ persistence/entities/product.go
@@ -11,7 +11,6 @@ import (
 
 // Product represents a safety product such as a motorcycle helmet, jacket, etc. It contains the price of the product, certifications, etc.
 type Product struct {
-	// TODO: fix this https://www.revzilla.com/motorcycle/revit-worker-overshirt?utm_source=CJ&utm_medium=affiliate&utm_campaign=8505854&PublisherName=CrashTested&cjevent=1de41d2f5aa911e9805c00640a1c0e0d
 	UUID                 uuid.UUID            `json:"uuid"`
 	ExternalID           string               `json:"externalID"`
 	Type                 string               `json:"type"`
