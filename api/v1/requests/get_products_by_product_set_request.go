5216ab187f0fac7a48a23dcd1a5fdef1d06f75a7
Benjamin Katz <bakatz91@gmail.com>
2019-12-28 19:39:49 -0800

    Removes unused file

--- api/v1/requests/get_products_by_product_set_request.go
+++
@@ -1,8 +0,0 @@
-package requests
-
-import "github.com/google/uuid"
-
-// GetProductSetDetails represents a request to get all of the products identified by this product set ID
-type GetProductSetDetails struct {
-	ProductSetID uuid.UUID `json:"productSetID"`
-}
